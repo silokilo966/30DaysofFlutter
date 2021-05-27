ab401d1ddf696b264c44164c1658a67a1f5fd3d3
silokilo966 <silokilo966@gmail.com>
2021-05-27 01:54:28 -0700

    day9 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,20 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
+
+final product = [
+  Item(
+      id: "Codepur",
+      name: "iPhone 12 Pro Max",
+      desc: "Apple iphone 12th Gen",
+      price: 999,
+      color: "#33505a",
+      image: "")
+];
