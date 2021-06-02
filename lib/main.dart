47483889e29157ae404d77f28369a63db991b2d5
silokilo966 <silokilo966@gmail.com>
2021-06-02 01:01:55 -0700

    day16 completed

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'pages/home_page.dart';
 void main() {
   runApp(MyApp());
 }
+//
 
 class MyApp extends StatelessWidget {
   @override
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -46,15 +46,48 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
-                //itemCount: CatalogModel.items.length, //no of items
-                itemCount: CatalogModel.items.length,
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                ),
                 itemBuilder: (context, index) {
-                  //how to show the items
-                  //return ItemWidget(item: CatalogModel.items[index]);
-                  return ItemWidget(item: CatalogModel.items[index]);
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                          header: Container(
+                            child: Text(
+                              item.name,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(color: Colors.deepPurple),
+                          ),
+                          footer: Container(
+                            child: Text(
+                              item.price.toString(),
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(color: Colors.black),
+                          ),
+                          child: Image.network(item.image)));
                 },
+                itemCount: CatalogModel.items.length,
               )
+            // ? ListView.builder(
+            //     //itemCount: CatalogModel.items.length, //no of items
+            //     itemCount: CatalogModel.items.length,
+            //     itemBuilder: (context, index) {
+            //       //how to show the items
+            //       //return ItemWidget(item: CatalogModel.items[index]);
+            //       return ItemWidget(item: CatalogModel.items[index]);
+            //     },
+            // )
             : Center(child: CircularProgressIndicator()),
       ),
       drawer: MyDrawer(),
